4cdb95a57723df7d869e3f7e5b806ed5674c50f5
Volodymyr Labunets <vplabunets@gmail.com>
2022-08-29 19:45:29 +0300

    29 08 2022 19 45

--- src/index.js
+++ src/index.js
@@ -4,48 +4,84 @@ let _ = require('lodash');
 const DEBOUNCE_DELAY = 300;
 const refs = {
   inputEl: document.querySelector('#search-box'),
-  divCountryEl: document.querySelector('.country-info'),
+  countryListEl: document.querySelector('.country-list'),
+  countryInfoEl: document.querySelector('.country-info'),
 };
 let aaaa = refs.inputEl.addEventListener(
   'input',
   _.debounce(onInputType, DEBOUNCE_DELAY)
 );
 
-console.log(refs.inputEl);
+// console.log(refs.inputEl);
 
 function onInputType(event) {
   //   event.preventDefault();
-  //   formData[e.target.name] = e.target.value;
+
   let inputData = event.target.value;
+  // console.log(event.target.value);
   inputData = inputData.trim();
-  console.log(inputData);
+  // console.log(inputData);
   fetchCountries(inputData);
   return inputData;
 }
 // console.log(onInputType());
 
 function fetchCountries(name) {
-  let countryLink = `https://restcountries.com/v2/name/${name}`;
+  // let countryLink = `https://restcountries.com/v2/name/${name},{name}`;
+  let countryLink = `https://restcountries.com/v2/{${name}}?fields={name.official},{capital},{population},{flags.svg},{languages}`;
+  // let countryLink = 'https://restcountries.com/v2/all?fields=name,capital,currencies'
+  // let countryLink = `https://restcountries.com/v2/all?fields=${name},capital,currencies`;
   const fetchedCountry = fetch(countryLink)
     .then(response => {
       return response.json();
     })
-    .then(countries => {
-      console.log(countries);
-      let names = countries.map(country => country.name);
-      console.log(names);
-
-      for (const nameOfCountry of names) {
-        // if (names.length >= 5) {
-        refs.divCountryEl.innerHTML = `<ul><li>${nameOfCountry}</li></ul>`;
-        // } else {
-        //   refs.divCountryEl.innerHTML = `<ul><li>${nameOfCountry}</li></ul>`;
-        //   console.log('нужно сделать другую разметку');
-        // }
-      }
-    })
+    .then(renderCountry)
     .catch(error => console.log(error));
   return fetchedCountry;
 }
 
 console.log(fetchCountries(onInputType()));
+
+function renderCountry(countries) {
+  // console.log(countries);
+  // let names = countries.map(country => country.name);
+  // console.log(names);
+  if (countries.length >= 10) {
+    return alert('сильно много хочешь');
+  } else if (countries.length >= 2 && countries.length <= 10) {
+    const markup = countries
+      .map(
+        nameOfCountry =>
+          `<li class="country-item"><img class="country-flag" src="${nameOfCountry.flags.svg}"
+           alt="flag ${nameOfCountry.name}" width = "30" height ="18"> 
+           <p class="country-name">${nameOfCountry.name}</p></li>`
+      )
+      .join('');
+    // console.log(markup);
+    // for (const nameOfCountry of names) {
+    // console.log(nameOfCountry);>
+    refs.countryListEl.innerHTML = markup;
+  } else {
+    refs.countryListEl.remove();
+    console.log('тут будет красота');
+
+    const markup2 = countries
+      .map(
+        nameOfCountry =>
+          `<div class="country-card"><img class="country-card__flag" src="${nameOfCountry.flags.svg}"
+           alt="flag ${nameOfCountry.name}" width = "30" height ="18"> <h2> ${nameOfCountry.name}</h2></div>
+          <li class="">Capital: ${nameOfCountry.capital} </li>
+          <li class="">Population: ${nameOfCountry.population}  </li>
+          <li class="">Languages: ${nameOfCountry.languages[0].name}  </li>
+          `
+      )
+      .join('');
+    refs.countryInfoEl.innerHTML = markup2;
+  }
+
+  // refs.countryInfoEl.innerHTML = markup2;
+  // refs.divCountryEl.innerHTML = `<ul><li>${nameOfCountry}</li></ul>`;
+  // console.log('нужно сделать другую разметку');
+  // }
+  // }
+}
