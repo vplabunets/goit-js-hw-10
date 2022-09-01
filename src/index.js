622e7c02cb402177f407bb2019d42280307f5e88
Volodymyr Labunets <vplabunets@gmail.com>
2022-09-01 22:51:51 +0300

    01 09 2022 22 51

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import fetchCountriesByName from './fetchCountries';
-import refs from './refs';
+// import refs from './refs';
 let _ = require('lodash');
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryListEl: document.querySelector('.country-list'),
+  countryInfoEl: document.querySelector('.country-info'),
+};
 const DEBOUNCE_DELAY = 300;
 const FAILURE_MESSAGE = 'Oops, there is no country with that name';
 const NOTIFICATION_MESSAGE =
   'Too many matches found. Please enter a more specific name.';
-refs().inputEl.addEventListener(
-  'input',
-  _.debounce(onInputType, DEBOUNCE_DELAY)
-);
+refs.inputEl.addEventListener('input', _.debounce(onInputType, DEBOUNCE_DELAY));
 
 function onInputType(event) {
   event.preventDefault();
@@ -26,24 +28,16 @@ function onInputType(event) {
 }
 
 function renderCountry(countries) {
-  // let { flags, capital, population, languages, name } = countries;
-  for (let key of countries) {
-    // console.log(key);
-  }
-  // console.log(countries);
-  console.log(countries);
-  console.log(typeof countries);
-
   if (countries.status === 404) {
-    refs().countryInfoEl.innerHTML = '';
+    refs.countryInfoEl.innerHTML = '';
     Notiflix.Notify.failure(FAILURE_MESSAGE);
   } else if (countries.length >= 10) {
-    refs().countryInfoEl.innerHTML = '';
+    refs.countryInfoEl.innerHTML = '';
     return Notiflix.Notify.info(NOTIFICATION_MESSAGE);
   } else if (countries.length >= 2 && countries.length <= 10) {
     // console.log(countries);
     // console.log(typeof countries);
-    refs().countryInfoEl.innerHTML = countries
+    refs.countryInfoEl.innerHTML = countries
       .map(
         ({ flags, name }) =>
           `<li class="country-item"><img class="country-flag" src="${flags.svg}"
@@ -55,7 +49,7 @@ function renderCountry(countries) {
 
     // refs().countryInfoEl.innerHTML = markup;
   } else {
-    refs().countryInfoEl.innerHTML = countries.map(
+    refs.countryInfoEl.innerHTML = countries.map(
       ({ flags, capital, population, languages, name }) =>
         `<div class="country-card"><img class="country-card__flag" src="${
           flags.svg
--- src/refs.js
+++ src/refs.js
@@ -1,8 +1,3 @@
 export default function refs() {
-  const refs = {
-    inputEl: document.querySelector('#search-box'),
-    countryListEl: document.querySelector('.country-list'),
-    countryInfoEl: document.querySelector('.country-info'),
-  };
   return refs;
 }
